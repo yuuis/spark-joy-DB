a85b53c36a90adfb52727660465d41c165d6a98f
Yu Ishiguro <yuuis.egr@gmail.com>
2019-03-21 15:19:32 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,40 +43,30 @@ Event.create(name: '人狼バンチ戦', date: Date.new(2019, 1, 1))
 Event.create(name: '消しゴムバトルバンチ戦', date: Date.new(2019, 2, 1))
 Event.create(name: '開発合宿バンチ戦', date: Date.new(2019, 3, 1))
 
+# event_1
 Picture.create(event_id: 1, taken_by: 5) # 1
 UserPicture.create(user_id: 1, picture_id: 1, smile_point: 0.8)
 UserPicture.create(user_id: 2, picture_id: 1, smile_point: 0.6)
 UserPicture.create(user_id: 3, picture_id: 1, smile_point: 0.9)
-UserReflect.create(user_id: 1, with: 2)
-UserReflect.create(user_id: 1, with: 3)
-UserReflect.create(user_id: 2, with: 1)
-UserReflect.create(user_id: 2, with: 3)
-UserReflect.create(user_id: 3, with: 1)
-UserReflect.create(user_id: 3, with: 2)
+[1, 2, 3].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
 
 Picture.create(event_id: 1, taken_by: 1) # 2
 UserPicture.create(user_id: 2, picture_id: 2, smile_point: 0.4)
 UserPicture.create(user_id: 4, picture_id: 2, smile_point: 0.7)
-UserReflect.create(user_id: 2, with: 4)
-UserReflect.create(user_id: 4, with: 2)
+[2, 4].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
 
 Picture.create(event_id: 1, taken_by: 6) # 3
 UserPicture.create(user_id: 1, picture_id: 3, smile_point: 0.8)
 UserPicture.create(user_id: 2, picture_id: 3, smile_point: 0.4)
 UserPicture.create(user_id: 3, picture_id: 3, smile_point: 0.6)
 UserPicture.create(user_id: 4, picture_id: 3, smile_point: 0.9)
-UserReflect.create(user_id: 1, with: 2)
-UserReflect.create(user_id: 1, with: 3)
-UserReflect.create(user_id: 1, with: 4)
-UserReflect.create(user_id: 2, with: 1)
-UserReflect.create(user_id: 2, with: 3)
-UserReflect.create(user_id: 2, with: 4)
-UserReflect.create(user_id: 3, with: 1)
-UserReflect.create(user_id: 3, with: 2)
-UserReflect.create(user_id: 3, with: 4)
-UserReflect.create(user_id: 4, with: 1)
-UserReflect.create(user_id: 4, with: 2)
-UserReflect.create(user_id: 4, with: 3)
+[1, 2, 3, 4].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
 
 Picture.create(event_id: 1, taken_by: 3) # 4
 UserPicture.create(user_id: 5, picture_id: 4, smile_point: 0.7)
@@ -84,54 +74,180 @@ UserPicture.create(user_id: 5, picture_id: 4, smile_point: 0.7)
 Picture.create(event_id: 1, taken_by: 1) # 5
 UserPicture.create(user_id: 5, picture_id: 5, smile_point: 0.7)
 UserPicture.create(user_id: 6, picture_id: 5, smile_point: 0.7)
-UserReflect.create(user_id: 5, with: 6)
-UserReflect.create(user_id: 6, with: 5)
+[5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
 
 Picture.create(event_id: 1, taken_by: 3) # 5
 UserPicture.create(user_id: 1, picture_id: 6, smile_point: 0.8)
 UserPicture.create(user_id: 5, picture_id: 6, smile_point: 0.7)
 UserPicture.create(user_id: 6, picture_id: 6, smile_point: 0.7)
-UserReflect.create(user_id: 1, with: 5)
-UserReflect.create(user_id: 1, with: 6)
-UserReflect.create(user_id: 5, with: 1)
-UserReflect.create(user_id: 5, with: 6)
-UserReflect.create(user_id: 6, with: 5)
-UserReflect.create(user_id: 6, with: 1)
+[1, 5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
 
 Picture.create(event_id: 1, taken_by: 3) # 6
 UserPicture.create(user_id: 2, picture_id: 6, smile_point: 0.8)
 UserPicture.create(user_id: 3, picture_id: 6, smile_point: 0.6)
 UserPicture.create(user_id: 5, picture_id: 6, smile_point: 0.7)
 UserPicture.create(user_id: 6, picture_id: 6, smile_point: 0.7)
-UserReflect.create(user_id: 2, with: 3)
-UserReflect.create(user_id: 2, with: 5)
-UserReflect.create(user_id: 2, with: 6)
-UserReflect.create(user_id: 3, with: 2)
-UserReflect.create(user_id: 3, with: 5)
-UserReflect.create(user_id: 3, with: 6)
-UserReflect.create(user_id: 5, with: 2)
-UserReflect.create(user_id: 5, with: 3)
-UserReflect.create(user_id: 5, with: 5)
-UserReflect.create(user_id: 6, with: 2)
-UserReflect.create(user_id: 6, with: 3)
-UserReflect.create(user_id: 6, with: 5)
+[2, 3, 5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
 
 Picture.create(event_id: 1, taken_by: 5) # 7
 UserPicture.create(user_id: 1, picture_id: 7, smile_point: 0.9)
 UserPicture.create(user_id: 6, picture_id: 7, smile_point: 0.8)
-UserReflect.create(user_id: 1, with: 6)
-UserReflect.create(user_id: 6, with: 1)
+[1, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
 
 Picture.create(event_id: 1, taken_by: 2) # 8
 UserPicture.create(user_id: 4, picture_id: 8, smile_point: 0.8)
 UserPicture.create(user_id: 5, picture_id: 8, smile_point: 0.6)
 UserPicture.create(user_id: 6, picture_id: 8, smile_point: 0.9)
-UserReflect.create(user_id: 4, with: 5)
-UserReflect.create(user_id: 4, with: 6)
-UserReflect.create(user_id: 5, with: 4)
-UserReflect.create(user_id: 5, with: 6)
-UserReflect.create(user_id: 6, with: 4)
-UserReflect.create(user_id: 6, with: 5)
+[4, 5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+
+# event_2
+Picture.create(event_id: 2, taken_by: 2) # 9
+UserPicture.create(user_id: 1, picture_id: 9, smile_point: 0.8)
+UserPicture.create(user_id: 3, picture_id: 9, smile_point: 0.6)
+UserPicture.create(user_id: 5, picture_id: 9, smile_point: 0.9)
+[1, 3, 5].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 2, taken_by: 1) # 10
+UserPicture.create(user_id: 2, picture_id: 10, smile_point: 0.4)
+UserPicture.create(user_id: 4, picture_id: 10, smile_point: 0.7)
+[2, 4].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 2, taken_by: 6) # 11
+UserPicture.create(user_id: 1, picture_id: 11, smile_point: 0.8)
+UserPicture.create(user_id: 2, picture_id: 11, smile_point: 0.4)
+UserPicture.create(user_id: 4, picture_id: 11, smile_point: 0.9)
+UserPicture.create(user_id: 5, picture_id: 11, smile_point: 0.6)
+[1, 2, 4, 5].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 2, taken_by: 3) # 12
+UserPicture.create(user_id: 1, picture_id: 12, smile_point: 0.7)
+
+Picture.create(event_id: 2, taken_by: 1) # 13
+UserPicture.create(user_id: 5, picture_id: 13, smile_point: 0.7)
+UserPicture.create(user_id: 6, picture_id: 13, smile_point: 0.6)
+[5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 2, taken_by: 6) # 14
+UserPicture.create(user_id: 4, picture_id: 14, smile_point: 0.7)
+UserPicture.create(user_id: 5, picture_id: 14, smile_point: 0.8)
+UserPicture.create(user_id: 6, picture_id: 14, smile_point: 0.7)
+[4, 5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 2, taken_by: 1) # 15
+UserPicture.create(user_id: 2, picture_id: 15, smile_point: 0.8)
+UserPicture.create(user_id: 3, picture_id: 15, smile_point: 0.6)
+UserPicture.create(user_id: 4, picture_id: 15, smile_point: 0.6)
+UserPicture.create(user_id: 5, picture_id: 15, smile_point: 0.7)
+UserPicture.create(user_id: 6, picture_id: 15, smile_point: 0.7)
+[2, 3, 4, 5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 2, taken_by: 5) # 16
+UserPicture.create(user_id: 1, picture_id: 16, smile_point: 0.9)
+UserPicture.create(user_id: 3, picture_id: 16, smile_point: 0.9)
+UserPicture.create(user_id: 6, picture_id: 16, smile_point: 0.8)
+[1, 3, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 2, taken_by: 3) # 17
+UserPicture.create(user_id: 4, picture_id: 17, smile_point: 0.8)
+UserPicture.create(user_id: 5, picture_id: 17, smile_point: 0.6)
+UserPicture.create(user_id: 6, picture_id: 17, smile_point: 0.9)
+[4, 5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+
+# event_3
+Picture.create(event_id: 3, taken_by: 6) # 18
+UserPicture.create(user_id: 1, picture_id: 18, smile_point: 0.8)
+UserPicture.create(user_id: 2, picture_id: 18, smile_point: 0.6)
+UserPicture.create(user_id: 5, picture_id: 18, smile_point: 0.9)
+[1, 2, 5].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 3, taken_by: 1) # 19
+UserPicture.create(user_id: 2, picture_id: 19, smile_point: 0.4)
+UserPicture.create(user_id: 4, picture_id: 19, smile_point: 0.4)
+UserPicture.create(user_id: 6, picture_id: 19, smile_point: 0.7)
+[2, 4, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 3, taken_by: 6) # 20
+UserPicture.create(user_id: 1, picture_id: 20, smile_point: 0.8)
+UserPicture.create(user_id: 5, picture_id: 20, smile_point: 0.6)
+[1, 5].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 3, taken_by: 3) # 21
+UserPicture.create(user_id: 1, picture_id: 21, smile_point: 0.7)
+
+Picture.create(event_id: 3, taken_by: 1) # 22
+UserPicture.create(user_id: 2, picture_id: 22, smile_point: 0.7)
+UserPicture.create(user_id: 6, picture_id: 22, smile_point: 0.6)
+[2, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 3, taken_by: 6) # 23
+UserPicture.create(user_id: 4, picture_id: 23, smile_point: 0.7)
+UserPicture.create(user_id: 5, picture_id: 23, smile_point: 0.8)
+UserPicture.create(user_id: 6, picture_id: 23, smile_point: 0.7)
+[4, 5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 3, taken_by: 1) # 24
+UserPicture.create(user_id: 2, picture_id: 24, smile_point: 0.8)
+UserPicture.create(user_id: 3, picture_id: 24, smile_point: 0.6)
+UserPicture.create(user_id: 4, picture_id: 24, smile_point: 0.6)
+UserPicture.create(user_id: 5, picture_id: 24, smile_point: 0.7)
+UserPicture.create(user_id: 6, picture_id: 24, smile_point: 0.7)
+[2, 3, 4, 5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 3, taken_by: 5) # 25
+UserPicture.create(user_id: 1, picture_id: 25, smile_point: 0.9)
+UserPicture.create(user_id: 3, picture_id: 25, smile_point: 0.9)
+UserPicture.create(user_id: 6, picture_id: 25, smile_point: 0.8)
+[1, 3, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
+
+Picture.create(event_id: 3, taken_by: 3) # 26
+UserPicture.create(user_id: 4, picture_id: 26, smile_point: 0.8)
+UserPicture.create(user_id: 5, picture_id: 26, smile_point: 0.6)
+UserPicture.create(user_id: 6, picture_id: 26, smile_point: 0.9)
+[4, 5, 6].permutation(2) do |p|
+  UserReflect.create(user_id: p[0], with: p[1])
+end
 
 AggregateLog.create(user_id: 1, event_id: 1, laugh_std: 0.4, rare_encount_point: 3, taken_picture_with_many_people_point: 7,
                     take_good_picture_point: 4, between_product_interact_point: 8, divercity_point: 7, score: 6)
@@ -144,13 +260,24 @@ AggregateLog.create(user_id: 4, event_id: 1, laugh_std: 0.2, rare_encount_point:
 AggregateLog.create(user_id: 5, event_id: 1, laugh_std: 0.4, rare_encount_point: 4, taken_picture_with_many_people_point: 5,
                     take_good_picture_point: 8, between_product_interact_point: 7, divercity_point: 8, score: 7)
 
-AggregateLog.create(user_id: 1, event_id: 2, laugh_std: 0.5, rare_encount_point: 4, taken_picture_with_many_people_point: 8,
+AggregateLog.create(user_id: 1, event_id: 2, laugh_std: 0.4, rare_encount_point: 4, taken_picture_with_many_people_point: 8,
                     take_good_picture_point: 3, between_product_interact_point: 9, divercity_point: 8, score: 6)
-AggregateLog.create(user_id: 2, event_id: 2, laugh_std: 0.3, rare_encount_point: 2, taken_picture_with_many_people_point: 8,
+AggregateLog.create(user_id: 2, event_id: 2, laugh_std: 0.5, rare_encount_point: 2, taken_picture_with_many_people_point: 8,
                     take_good_picture_point: 8, between_product_interact_point: 7, divercity_point: 8, score: 7)
-AggregateLog.create(user_id: 3, event_id: 2, laugh_std: 0.4, rare_encount_point: 4, taken_picture_with_many_people_point: 5,
+AggregateLog.create(user_id: 3, event_id: 2, laugh_std: 0.3, rare_encount_point: 4, taken_picture_with_many_people_point: 5,
                     take_good_picture_point: 8, between_product_interact_point: 7, divercity_point: 8, score: 7)
-AggregateLog.create(user_id: 4, event_id: 2, laugh_std: 0.4, rare_encount_point: 3, taken_picture_with_many_people_point: 7,
+AggregateLog.create(user_id: 4, event_id: 2, laugh_std: 0.2, rare_encount_point: 3, taken_picture_with_many_people_point: 7,
                     take_good_picture_point: 4, between_product_interact_point: 8, divercity_point: 7, score: 6)
-AggregateLog.create(user_id: 5, event_id: 2, laugh_std: 0.2, rare_encount_point: 1, taken_picture_with_many_people_point: 6,
+AggregateLog.create(user_id: 5, event_id: 2, laugh_std: 0.5, rare_encount_point: 1, taken_picture_with_many_people_point: 6,
                     take_good_picture_point: 7, between_product_interact_point: 9, divercity_point: 5, score: 5)
+
+AggregateLog.create(user_id: 1, event_id: 3, laugh_std: 0.4, rare_encount_point: 6, taken_picture_with_many_people_point: 9,
+                    take_good_picture_point: 6, between_product_interact_point: 6, divercity_point: 7, score: 6)
+AggregateLog.create(user_id: 2, event_id: 3, laugh_std: 0.5, rare_encount_point: 6, taken_picture_with_many_people_point: 8,
+                    take_good_picture_point: 5, between_product_interact_point: 9, divercity_point: 9, score: 9)
+AggregateLog.create(user_id: 3, event_id: 3, laugh_std: 0.3, rare_encount_point: 2, taken_picture_with_many_people_point: 8,
+                    take_good_picture_point: 8, between_product_interact_point: 7, divercity_point: 8, score: 7)
+AggregateLog.create(user_id: 4, event_id: 3, laugh_std: 0.2, rare_encount_point: 1, taken_picture_with_many_people_point: 6,
+                    take_good_picture_point: 6, between_product_interact_point: 9, divercity_point: 5, score: 7)
+AggregateLog.create(user_id: 5, event_id: 3, laugh_std: 0.4, rare_encount_point: 4, taken_picture_with_many_people_point: 5,
+                    take_good_picture_point: 3, between_product_interact_point: 7, divercity_point: 8, score: 7)
